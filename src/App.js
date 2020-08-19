4e9e049e5f06c60bc60642765573ad401065e978
Brittney Bannister <bbannister1127@icloud.com>
2020-08-19 17:27:24 -0400

    have to do styling-having trouble styling. coming back later to figure it out.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Button, Card} from 'react-bootstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//TODO: replace with actual username
+const GITHUB_USER = "https://api.github.com/users/BrittBannister"
+
+//const GITHUB_USER = (githubUserName) => 'https://api.github.com/users/${githubuserName}
+
+class App extends React.Component {
+  state = {
+    users: {},
+    active: false
+  }
+  
+    handleTooggle = (evt) => {
+      if(this.state.active === false) {
+        this.setState({active: false})
+      } else {
+        this.setState({active: true})
+      }
+      console.log('its working')
+      fetch(GITHUB_USER) //(GITHUB_USER ('ivey'))
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        this.setState({user: data, active: true})
+      })
+    }
+
+ 
+// TODO render avatar_url, name, 2 other pieces of info from them
+  render() {
+    return (
+      <>
+      <Button onClick = {this.handleTooggle}>Toggle Users.</Button>
+      {this.state.active === true && (
+        <Card>
+        <Card.Body>
+        {/* <img src = {this.state.user.avatar_url} alt = ''/> */}
+        <Card.Img variant = 'top' src = {this.state.user.avatar_url} />
+          <Card.Title>{this.state.user.login}</Card.Title>
+          <Card.Text>{this.state.user.bio}</Card.Text>
+          {/* <Card description = {this.state.user.bio} /> */}
+        </Card.Body>
+         
+        </Card>
+
+      )}
+      </>
+    )
+  }
 }
 
 export default App;
