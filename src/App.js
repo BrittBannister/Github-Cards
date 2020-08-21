34b821ecf4f07b777ff4f9bd95b376c8af598760
Brittney Bannister <bbannister1127@icloud.com>
2020-08-21 10:37:21 -0400

    transformed it to using hooks

--- src/App.js
+++ src/App.js
@@ -1,60 +1,104 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import { Button } from 'semantic-ui-react'
 import {Card, Icon, Image} from 'semantic-ui-react'
 
 
+
 const GITHUB_USER = "https://api.github.com/users/BrittBannister"
 
+  function App() {
+    const [users, setUsers] = useState({})
+    const [active, setActive] = useState(false)
 
-class App extends React.Component {
-  state = {
-    users: {},
-    active: false
-  }
-
-    handleTooggle = () => {
+     const handleTooggle = () => {
       console.log('it works')
-      if(this.state.active === false) {
+      if(active === false) {
         fetch(GITHUB_USER)
         .then((response) => response.json())
         .then((data) => {
           console.log(data)
-          this.setState({
-            user: data,
-            active: true
-          })
+        setUsers(data)
+        setActive(true)
         })
       } else {
-        // this.setState({user: data, active: false})
-        this.setState({active: false})
+        setActive(false)
       }
     }
-
-    render(){
+    
       return(
         <React.Fragment>
-        <Button onClick = {this.handleTooggle}>Toggle</Button>
-        {this.state.active && (
+        <Button onClick = {handleTooggle}>Toggle</Button>
+        {active && (
           <React.Fragment>
           <Card>
-         <Image src = {this.state.user.avatar_url} />
+         <Image src = {users.avatar_url} />
          <Card.Content>
-           <Card.Header>{this.state.user.name}</Card.Header>
-           <Card.Meta>{this.state.user.bio}</Card.Meta>
+           <Card.Header>{users.name}</Card.Header>
+           <Card.Meta>{users.bio}</Card.Meta>
            <br />
-           <Card.Meta>{this.state.user.location}</Card.Meta>
+           <Card.Meta>{users.location}</Card.Meta>
          </Card.Content>
         <Card.Content extra>
-          <Icon name = 'user' />{this.state.user.followers}{' '}followers
+          <Icon name = 'user' />{users.followers}{' '}followers
         </Card.Content>
        </Card>
         </React.Fragment>
         )}
         </React.Fragment>
       )
-    }
-  }
+        }
+  
   export default App
+
+  // class App extends React.Component {
+  // state = {
+  //   users: {},
+  //   active: false
+  // }
+
+  //   handleTooggle = () => {
+  //     console.log('it works')
+  //     if(this.state.active === false) {
+  //       fetch(GITHUB_USER)
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         console.log(data)
+  //         this.setState({
+  //           user: data,
+  //           active: true
+  //         })
+  //       })
+  //     } else {
+  //       // this.setState({user: data, active: false})
+  //       this.setState({active: false})
+  //     }
+  //   }
+
+  //   render(){
+  //     return(
+  //       <React.Fragment>
+  //       <Button onClick = {this.handleTooggle}>Toggle</Button>
+  //       {this.state.active && (
+  //         <React.Fragment>
+  //         <Card>
+  //        <Image src = {this.state.user.avatar_url} />
+  //        <Card.Content>
+  //          <Card.Header>{this.state.user.name}</Card.Header>
+  //          <Card.Meta>{this.state.user.bio}</Card.Meta>
+  //          <br />
+  //          <Card.Meta>{this.state.user.location}</Card.Meta>
+  //        </Card.Content>
+  //       <Card.Content extra>
+  //         <Icon name = 'user' />{this.state.user.followers}{' '}followers
+  //       </Card.Content>
+  //      </Card>
+  //       </React.Fragment>
+  //       )}
+  //       </React.Fragment>
+  //     )
+  //   }
+  // }
+  // export default App
  
 
